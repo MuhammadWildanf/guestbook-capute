b0e31bfe79eb43bb1aa607b94c90da929abcecec
Muhammad Faiz Satrio <faizsatrio890@gmail.com>
2024-06-24 20:06:54 +0700

    first-commit

---
+++ frontend/script.js
@@ -0,0 +1,97 @@
+$(document).ready(function () {
+    $("#country").select2({
+        placeholder: "Select Country",
+        allowClear: true,
+    });
+});
+
+var key
+
+document.getElementById("submit").addEventListener('click', e => {
+    e.preventDefault()
+    var name = document.getElementById("name").value
+    var email = document.getElementById("email").value
+    var country = document.getElementById("country").value
+    var comment = document.getElementById("comment").value
+
+    console.log(name, email, country, comment)
+
+    document.getElementById("p2").style.display = 'flex'
+    document.getElementById("p1").style.display = 'none'
+
+    Swal.fire({
+        icon: "info",
+        title: "Thank You",
+        text: "Something went wrong!",
+    });
+
+    updateData(key, name, email, country, comment)
+})
+
+document.getElementById("next").addEventListener('click', e => {
+    e.preventDefault()
+    var name = document.getElementById("name").value
+    var email = document.getElementById("email").value
+    var country = document.getElementById("country").value
+    var comment = document.getElementById("comment").value
+
+    console.log(name, email, country, comment)
+
+    document.getElementById("pg2").style.display = 'block'
+    document.getElementById("pg1").style.display = 'none'
+
+    Swal.fire({
+        icon: "info",
+        title: "Oops...",
+        text: "Something went wrong!",
+    });
+
+    submit(name, email, country, comment)
+})
+
+async function submit(name, email, country, comment) {
+    try {
+        const response = await fetch('http://localhost:3000/submit-form', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, country, comment })
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const responseData = await response.json();
+        key = responseData.key
+        console.log('Response Data:', responseData.key);
+
+    } catch (error) {
+        console.error('Error:', error);
+        console.log('Error submitting data');
+    }
+}
+
+async function updateData(key, name, email, country, comment) {
+    try {
+        const response = await fetch('http://localhost:3000/update-form', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ key, name, email, country, comment })
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const responseData = await response.json();
+        console.log('Response Data:', responseData.msg);
+
+    } catch (error) {
+        console.error('Error:', error);
+        console.log('Error submitting data');
+    }
+}
---
+++ index.js
@@ -0,0 +1,58 @@
+const cors = require('cors')
+const express = require('express');
+const admin = require('firebase-admin');
+const serviceAccount = require('./config/serviceAccountKey.json');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://infocomm-bangkok-default-rtdb.asia-southeast1.firebasedatabase.app'
+});
+
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
+app.use(express.json())
+
+app.use(bodyParser.json());
+
+app.use(cors({ origin: true }));
+
+app.post('/submit-form', async (req, res) => {
+  try {
+    const db = admin.database()
+    const { name, email, country, comment } = req.body;
+    const timestamp = admin.database.ServerValue.TIMESTAMP;
+    const ref = db.ref('guest');
+    const newRef = await ref.push({ name, email, country, comment, timestamp })
+    const newKey = newRef.key
+    res.status(200).json({ key: newKey });
+  } catch (error) {
+    console.error('Error submitting data:', error);
+    res.status(500).send('Error submitting data');
+  }
+});
+
+app.post('/update-form', async (req, res) => {
+  try {
+    const db = admin.database()
+    const { key, name, email, country, comment } = req.body;
+    const ref = db.ref(`/guest/${key}`);
+    const timestamp = admin.database.ServerValue.TIMESTAMP;
+    await ref.update({ name, email, country, comment, timestamp });
+    res.status(200).json({ msg: "Data Updated Successfully" });
+  } catch (error) {
+    console.error('Error updating data:', error);
+    res.status(500).send('Error updating data');
+  }
+});
